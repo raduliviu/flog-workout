1af206ac8fdbf998c55edd5ee921bfd11a71ac7c
Radu Baltag <baltag.radu@gmail.com>
2026-01-25 12:12:03 +0100

    added daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 function App() {
     return (
         <>
-            <header className="flex items-center justify-center">
-                <h1>Flog Workout</h1>
-            </header>
             <main className="flex flex-1 items-center justify-center">
-                <button onClick={() => console.log("Creating workout...")}>+ Create Workout</button>
+                <button className="btn" onClick={() => console.log("Creating workout...")}>
+                    + Create Workout
+                </button>
             </main>
         </>
     )
