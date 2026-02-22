a52a193fd69ff9b5783eaa604414ef329750bc8e
Radu Baltag <baltag.radu@gmail.com>
2026-02-22 14:16:05 +0100

    add exercise to workout functionality

--- src/routes/workouts/$workoutId.tsx
+++ src/routes/workouts/$workoutId.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, Link, useParams } from "@tanstack/react-router"
-import { dummyWorkouts } from "../../dummyData"
-import { ArrowBigLeft } from "lucide-react"
+import { dummyExercises, dummyWorkouts } from "../../dummyData"
+import { ArrowBigLeft, Plus } from "lucide-react"
+import { useRef, useState } from "react"
 
 export const Route = createFileRoute("/workouts/$workoutId")({
     component: RouteComponent,
@@ -9,7 +10,25 @@ export const Route = createFileRoute("/workouts/$workoutId")({
 function RouteComponent() {
     const { workoutId } = useParams({ from: "/workouts/$workoutId" })
     const selectedWorkout = dummyWorkouts.find((workout) => String(workout.id) === workoutId)
-    const exercises = selectedWorkout?.exercises || []
+
+    const [exercises, setExercises] = useState([...(selectedWorkout?.exercises || [])])
+    const modalRef = useRef<HTMLDialogElement>(null)
+    const [exerciseName, setExerciseName] = useState("")
+
+    const handleAddExercise = () => {
+        const trimmedName = exerciseName.trim()
+        if (!trimmedName) return
+
+        const newId = dummyExercises.length + 1
+        const newExercise = { id: newId, name: trimmedName }
+
+        dummyExercises.push(newExercise)
+        selectedWorkout?.exercises.push(newExercise)
+
+        setExercises((prev) => [...prev, newExercise])
+        setExerciseName("")
+        modalRef.current?.close()
+    }
 
     return (
         <>
@@ -26,6 +45,50 @@ function RouteComponent() {
                     </div>
                 </div>
             ))}
+            <button className="btn" onClick={() => modalRef.current?.showModal()}>
+                <Plus />
+                Add
+            </button>
+
+            <dialog id="addExerciseModal" ref={modalRef} className="modal" onClose={() => setExerciseName("")}>
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Add Exercise</h3>
+                    <fieldset className="fieldset py-4">
+                        <legend className="fieldset-legend">Exercise Name</legend>
+                        <input
+                            type="text"
+                            className="input validator w-full"
+                            placeholder="e.g. Barbell Row"
+                            value={exerciseName}
+                            onChange={(e) => setExerciseName(e.target.value)}
+                            onKeyDown={(e) => {
+                                if (e.key === "Enter") {
+                                    e.preventDefault()
+                                    handleAddExercise()
+                                }
+                            }}
+                            required
+                            minLength={3}
+                        />
+                        <p className="validator-hint">Must be at least 3 characters long</p>
+                    </fieldset>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            <button className="btn">Cancel</button>
+                        </form>
+                        <button
+                            disabled={exerciseName.length < 3}
+                            className="btn btn-primary"
+                            onClick={handleAddExercise}
+                        >
+                            Add Exercise
+                        </button>
+                    </div>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
         </>
     )
 }
